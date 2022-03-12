d2ce7e394a2c61310d140872cae5bb2ed23b5075
BuildTools <unconfigured@null.spigotmc.org>
2022-03-12 04:06:16 +0000

    add files

---
+++ src/main/java/org/spigotmc/AsyncCatcher.java
@@ -0,0 +1,13 @@
+package org.spigotmc;
+
+import net.minecraft.server.MinecraftServer;
+
+public class AsyncCatcher
+{
+
+    public static boolean enabled = true;
+
+    public static void catchOp(String reason)
+    {
+    }
+}
