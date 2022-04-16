e9d76d2eeba87cd2deb21f1c58465b8ab0861242
BuildTools <unconfigured@null.spigotmc.org>
2022-04-17 07:42:36 +0800

    remove locks

--- src/main/java/net/minecraftforge/fml/common/eventhandler/EventBus.java
+++ src/main/java/net/minecraftforge/fml/common/eventhandler/EventBus.java
@@ -43,115 +43,91 @@ import com.google.common.collect.Sets;
 import com.google.common.reflect.TypeToken;
 import org.bukkit.Bukkit;
 
-public class EventBus implements IEventExceptionHandler
-{
+public class EventBus implements IEventExceptionHandler {
     private static int maxID = 0;
 
     private ConcurrentHashMap<Object, ArrayList<IEventListener>> listeners = new ConcurrentHashMap<Object, ArrayList<IEventListener>>();
-    private Map<Object,ModContainer> listenerOwners = new MapMaker().weakKeys().weakValues().makeMap();
+    private Map<Object, ModContainer> listenerOwners = new MapMaker().weakKeys().weakValues().makeMap();
     private final int busID = maxID++;
     private IEventExceptionHandler exceptionHandler;
     private boolean shutdown;
 
-    public EventBus()
-    {
+    public EventBus() {
         ListenerList.resize(busID + 1);
         exceptionHandler = this;
     }
 
-    public EventBus(@Nonnull IEventExceptionHandler handler)
-    {
+    public EventBus(@Nonnull IEventExceptionHandler handler) {
         this();
         Preconditions.checkNotNull(handler, "EventBus exception handler can not be null");
         exceptionHandler = handler;
     }
-    Lock lock3 = new ReentrantLock();
-    public void register(Object target)
-    {
-        lock3.lock();
-        try{
-            if (listeners.containsKey(target))
-            {
-                return;
-            }
 
-            ModContainer activeModContainer = Loader.instance().activeModContainer();
-            if (activeModContainer == null)
-            {
-                FMLLog.log.error("Unable to determine registrant mod for {}. This is a critical error and should be impossible", target, new Throwable());
-                activeModContainer = Loader.instance().getMinecraftModContainer();
-            }
-            listenerOwners.put(target, activeModContainer);
-            boolean isStatic = target.getClass() == Class.class;
-            @SuppressWarnings("unchecked")
-            Set<? extends Class<?>> supers = isStatic ? Sets.newHashSet((Class<?>)target) : TypeToken.of(target.getClass()).getTypes().rawTypes();
-            for (Method method : (isStatic ? (Class<?>)target : target.getClass()).getMethods())
-            {
-                if (isStatic && !Modifier.isStatic(method.getModifiers()))
-                    continue;
-                else if (!isStatic && Modifier.isStatic(method.getModifiers()))
-                    continue;
+    public void register(Object target) {
+        if (listeners.containsKey(target)) {
+            return;
+        }
 
-                for (Class<?> cls : supers)
-                {
-                    try
-                    {
-                        Method real = cls.getDeclaredMethod(method.getName(), method.getParameterTypes());
-                        if (real.isAnnotationPresent(SubscribeEvent.class))
-                        {
-                            Class<?>[] parameterTypes = method.getParameterTypes();
-                            if (parameterTypes.length != 1)
-                            {
-                                throw new IllegalArgumentException(
-                                        "Method " + method + " has @SubscribeEvent annotation, but requires " + parameterTypes.length +
-                                                " arguments.  Event handler methods must require a single argument."
-                                );
-                            }
+        ModContainer activeModContainer = Loader.instance().activeModContainer();
+        if (activeModContainer == null) {
+            FMLLog.log.error("Unable to determine registrant mod for {}. This is a critical error and should be impossible", target, new Throwable());
+            activeModContainer = Loader.instance().getMinecraftModContainer();
+        }
+        listenerOwners.put(target, activeModContainer);
+        boolean isStatic = target.getClass() == Class.class;
+        @SuppressWarnings("unchecked")
+        Set<? extends Class<?>> supers = isStatic ? Sets.newHashSet((Class<?>) target) : TypeToken.of(target.getClass()).getTypes().rawTypes();
+        for (Method method : (isStatic ? (Class<?>) target : target.getClass()).getMethods()) {
+            if (isStatic && !Modifier.isStatic(method.getModifiers()))
+                continue;
+            else if (!isStatic && Modifier.isStatic(method.getModifiers()))
+                continue;
 
-                            Class<?> eventType = parameterTypes[0];
-
-                            if (!Event.class.isAssignableFrom(eventType))
-                            {
-                                throw new IllegalArgumentException("Method " + method + " has @SubscribeEvent annotation, but takes a argument that is not an Event " + eventType);
-                            }
-
-                            register(eventType, target, real, activeModContainer);
-                            break;
+            for (Class<?> cls : supers) {
+                try {
+                    Method real = cls.getDeclaredMethod(method.getName(), method.getParameterTypes());
+                    if (real.isAnnotationPresent(SubscribeEvent.class)) {
+                        Class<?>[] parameterTypes = method.getParameterTypes();
+                        if (parameterTypes.length != 1) {
+                            throw new IllegalArgumentException(
+                                    "Method " + method + " has @SubscribeEvent annotation, but requires " + parameterTypes.length +
+                                            " arguments.  Event handler methods must require a single argument."
+                            );
                         }
+
+                        Class<?> eventType = parameterTypes[0];
+
+                        if (!Event.class.isAssignableFrom(eventType)) {
+                            throw new IllegalArgumentException("Method " + method + " has @SubscribeEvent annotation, but takes a argument that is not an Event " + eventType);
+                        }
+
+                        register(eventType, target, real, activeModContainer);
+                        break;
                     }
-                    catch (NoSuchMethodException e)
-                    {
-                        ; // Eat the error, this is not unexpected
-                    }
+                } catch (NoSuchMethodException e) {
+                    ; // Eat the error, this is not unexpected
                 }
             }
-        }finally {
-            lock3.unlock();
         }
-    }
-    Lock lock2 = new ReentrantLock();
-    private void register(Class<?> eventType, Object target, Method method, final ModContainer owner)
-    {
 
-        lock2.lock();
-        try
-        {
+    }
+
+    private void register(Class<?> eventType, Object target, Method method, final ModContainer owner) {
+
+        try {
             Constructor<?> ctr = eventType.getConstructor();
             ctr.setAccessible(true);
-            Event event = (Event)ctr.newInstance();
+            Event event = (Event) ctr.newInstance();
             final ASMEventHandler asm = new ASMEventHandler(target, method, owner, IGenericEvent.class.isAssignableFrom(eventType));
 
             IEventListener listener = asm;
-            if (IContextSetter.class.isAssignableFrom(eventType))
-            {
-                listener = new IEventListener()
-                {
+            if (IContextSetter.class.isAssignableFrom(eventType)) {
+                listener = new IEventListener() {
                     @Override
-                    public void invoke(Event event)
-                    {
+                    public void invoke(Event event) {
                         ModContainer old = Loader.instance().activeModContainer();
                         Loader.instance().setActiveModContainer(owner);
-                        ((IContextSetter)event).setModContainer(owner);
+                        ((IContextSetter) event).setModContainer(owner);
                         asm.invoke(event);
                         Loader.instance().setActiveModContainer(old);
                     }
@@ -162,17 +138,14 @@ public class EventBus implements IEventExceptionHandler
 
             ArrayList<IEventListener> others = listeners.computeIfAbsent(target, k -> new ArrayList<>());
             others.add(listener);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             FMLLog.log.error("Error registering event handler: {} {} {}", owner, eventType, method, e);
-        }finally{
-            lock2.unlock();
         }
     }
+
     Lock lock1 = new ReentrantLock();
+
     public void unregister(Object object) {
-        lock1.lock();
         try {
             ArrayList<IEventListener> list = listeners.remove(object);
             if (list == null)
@@ -180,13 +153,14 @@ public class EventBus implements IEventExceptionHandler
             for (IEventListener listener : list) {
                 ListenerList.unregisterAll(busID, listener);
             }
-        }finally {
-            lock1.unlock();
+        } catch (Exception e) {
+            FMLLog.log.error("Error unregistering event handler: {}", object, e);
         }
     }
+
     Lock lockInkove = new ReentrantLock();
-    public boolean post(Event event)
-    {
+
+    public boolean post(Event event) {
 
         if (shutdown) return false;
 
@@ -198,38 +172,28 @@ public class EventBus implements IEventExceptionHandler
 
         IEventListener[] listeners = event.getListenerList().getListeners(busID);
         int index = 0;
-        try
-        {
-            lockInkove.lock();
-            for (; index < listeners.length; index++)
-            {
+        try {
+            for (; index < listeners.length; index++) {
                 listeners[index].invoke(event);
             }
-        }
-        catch (Throwable throwable)
-        {
+        } catch (Throwable throwable) {
             exceptionHandler.handleException(this, event, listeners, index, throwable);
             Throwables.throwIfUnchecked(throwable);
             throw new RuntimeException(throwable);
-        }finally {
-            lockInkove.unlock();
         }
         return event.isCancelable() && event.isCanceled();
     }
 
-    public void shutdown()
-    {
+    public void shutdown() {
         FMLLog.log.warn("EventBus {} shutting down - future events will not be posted.", busID);
         shutdown = true;
     }
 
     @Override
-    public void handleException(EventBus bus, Event event, IEventListener[] listeners, int index, Throwable throwable)
-    {
+    public void handleException(EventBus bus, Event event, IEventListener[] listeners, int index, Throwable throwable) {
         FMLLog.log.error("Exception caught during firing event {}:", event, throwable);
         FMLLog.log.error("Index: {} Listeners:", index);
-        for (int x = 0; x < listeners.length; x++)
-        {
+        for (int x = 0; x < listeners.length; x++) {
             FMLLog.log.error("{}: {}", x, listeners[x]);
         }
     }
